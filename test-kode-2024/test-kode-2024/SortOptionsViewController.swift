d3bbf6bbe0a57bf2d22355d042918de6664d884b
kirill kurochkin <godoffirekirill@gmail.com>
2024-03-22 11:25:12 +0200

    add filter
    create sordConcroller , add sort func in viewModel

---
+++ test-kode-2024/test-kode-2024/SortOptionsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SortOptionsViewController.swift
+//  test-kode-2024
+//
+//  Created by Кирилл Курочкин on 21.03.2024.
+//
+
+import UIKit
+
+class SortOptionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
