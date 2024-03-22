4799002df82fc57ac5a6f4daf0ec4720c850d0eb
kirill kurochkin <godoffirekirill@gmail.com>
2024-03-22 15:36:34 +0200

    final

---
+++ test-kode-2024/test-kode-2024/CriticalErrorViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CriticalErrorViewController.swift
+//  test-kode-2024
+//
+//  Created by Кирилл Курочкин on 22.03.2024.
+//
+
+import UIKit
+
+class CriticalErrorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ test-kode-2024/test-kode-2024/LoadErrorView.swift
@@ -0,0 +1,20 @@
+//
+//  LoadErrorView.swift
+//  test-kode-2024
+//
+//  Created by Кирилл Курочкин on 22.03.2024.
+//
+
+import UIKit
+
+class LoadErrorView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
